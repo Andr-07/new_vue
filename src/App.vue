de8108fe5167592bb90719b9924d343ecbe02fb8
Andrei Lubochnikov <aa.lubochnikov@gmail.com>
2019-09-26 09:16:35 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="sample">
+        <app-question
+        :title="questions[current].title"
+        :answers="questions[current].answers"
+        :current="current">
+        </app-question>
+    </div>
+</template>
+
+<script>
+import AppQuestion from './components/Question'
+export default {
+    components: {
+        AppQuestion
+    },
+    data() {
+        return {
+            questions: getData(),
+            current: 0
+        }
+    }
+    
+}
+
+function getData() {
+    return [{
+        title: "How old are you?",
+        answers: [{
+                answer: 15,
+                type: true
+            },
+            {
+                answer: 16,
+                type: false
+            }]
+},
+{
+        title: "What is your name?",
+        answers: [{
+                answer: "Andrey",
+                type: true
+            },
+            {
+                answer: "Oleg",
+                type: false
+            }]
+}
+]
+}
+</script>
---
+++ src/components/Question.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <form>
+            {{title}}
+            <hr>
+            <div v-for="answer in answers">
+                <input type="radio" name="radio" @change="sendData(answer.type)">
+                <label>{{answer.answer}}</label>
+            </div>
+            <button :disabled="buttonDisabled" class="btn btn-success" @click.prevent="newQuestion(current)">Send</button>
+        </form>
+        {{check}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title', 'answers', 'current'],
+    data() {
+        return {
+            check: null,
+            buttonDisabled: true,
+        }
+    },
+    methods: {
+        sendData(data) {
+            this.check = data;
+            this.buttonDisabled = false;
+        },
+        newQuestion(x) {
+            current++;
+            this.$emit('changevalue', this.current)
+        }
+    }
+}
+</script>
